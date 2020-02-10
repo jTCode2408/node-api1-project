14210d2fc851f198a179dac26b5d1a414e27e8b1
Jasmine Terry <jasmine.terry91@gmail.com>
2020-02-10 15:29:45 -0500

    GET all users end working

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ server.get('/', (req,res)=>{
     res.json({start:'Initial Server Start'})
 })
 
+//get all users api/users
+server.get('/api/users', (req,res)=>{
+    Users.find()
+    .then(users =>{
+        res.status(200).json({users});
+
+    })
+    .catch(err=>{
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    })
+})
+
+
+
+
+
+
+
+
+
 const port = 5000;
 server.listen(port, ()=>console.log(`\n** API on port ${port}\n`));
