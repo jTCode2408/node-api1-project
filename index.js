a696805b2e60d470553ffb97f57ad98168b7c0a8
Jasmine Terry <jasmine.terry91@gmail.com>
2020-02-10 15:07:47 -0500

    dependencies added. express added. initial server API created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express'); //import express
+const Users = require('./data/db'); //var for users data 
+const server=express();
+
+server.get('/', (req,res)=>{
+    res.json({start:'Initial Server Start'})
+})
+
+const port = 5000;
+server.listen(port, ()=>console.log(`\n** API on port ${port}\n`));
