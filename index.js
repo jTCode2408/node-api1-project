65eee409afe62219e46651736c244b5b4e12c32b
Jasmine Terry <jasmine.terry91@gmail.com>
2020-02-10 20:57:58 -0500

    findbyID error fixed

--- index.js
+++ index.js
@@ -24,39 +24,38 @@ server.post('/api/users', (req,res)=>{
     Users.insert(userData)
     .then(user=>{
         console.log(userData);
-        if (userData.name || userData.bio) {
+        if (userData.name && userData.bio) {
         res.status(201).json({
             name:userData.name,
             bio:userData.bio,
             created_at:Date.now(),
             udpated_at:Date.now()
         })
-} else {
+} else if (!userData.name || !userData.bio) {
     res.status(400).json({errorMessage: "Please provide name and bio for the user." })
 }
 })
 .catch(err => {
     console.log(err);
+    
     res.status(500).json({ errorMessage: "There was an error while saving the user to the database"});
   });
 
 })
 //get user by id(/api/users:id, findById)
 server.get('/api/users/:id', (req,res)=>{
-    // const {id} = req.params;
-    Users.findById(req.params.id)
+    const {id} = req.params;
+    Users.findById(id)
     .then(userId =>{
+      if (userId){
         res.status(200).json(userId)
-       
+      } else {
+        res.status(404).json({errorMessage:"user with specified ID does not exist"})
+      }
     })
     .catch(err=>{
-        console.log(err);
-        if (!id){
-            res.status(404).json({errorMessage:"user with specified ID does not exist"})
-            } else{
-              
+        console.log(err); 
         res.status(500).json({errorMessage: "The user information could not be retrieved." });
-            }
 
     })
 })
@@ -71,8 +70,9 @@ server.delete('/api/users/:id', (req,res)=>{
     .catch(err=>{
     console.log(err);
     if (!id){
-    res.status(404).json({ message: "The user with the specified ID does not exist."})
-    } else{
+        res.status(404).json({ message: "The user with the specified ID does not exist."})
+        } else {
+     
     res.status(500).json({errorMessage: "The user could not be removed"})
         }
     })
@@ -80,8 +80,8 @@ server.delete('/api/users/:id', (req,res)=>{
 
 //PUT.update(): accepts two arguments,id of the user to update, & object with the changes to apply.If the count is 1 it means the record was updated correctly.
 server.put('/api/users/:id', (req,res)=>{
-    let {name, bio} = req.body
-    Users.update(req.params.id, {name, bio})
+    const editData = req.body
+    Users.update(req.params.id, (editData))
     .then(edited=>{
         res.status(201).json(edited)
     })
@@ -89,7 +89,7 @@ server.put('/api/users/:id', (req,res)=>{
         console.log(err);
         if(!id){
             res.status(404).json({ message: "The user with the specified ID does not exist."})
-        } else if (!name || !bio){
+        } else if (!editData.name || !editData.bio){
             res.status(400).json({ message: "Please provide name and bio for the user."})
         } else{
             res.status(500).json({ message: "The user information could not be modified."})
